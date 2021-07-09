4726e6a8bb11b6187a789ff17379d86e64036185
KRP96 <kavindapeiris1996@gmail.com>
2021-07-09 12:34:04 +0530

    create class and init function

---
+++ Job_vacancies.py
@@ -0,0 +1,11 @@
+class Vacancy():
+    def __init__(self, accountant_vacancy, software_developer_vacancy, manager_vacancy, director_vacancy):
+        self.accountant = accountant_vacancy
+        self.software_developer = software_developer_vacancy
+        self.manager = manager_vacancy
+        self.director = director_vacancy
+        self.job_appliers = []
+
+
+
+        

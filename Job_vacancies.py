be2bac410b00c99d1f411f6f4c13265da445f089
KRP96 <kavindapeiris1996@gmail.com>
2021-07-09 12:59:34 +0530

    added descriptions using comments

--- Job_vacancies.py
+++ Job_vacancies.py
@@ -1,14 +1,19 @@
 class Vacancy():
     def __init__(self, accountant_vacancy, software_developer_vacancy, manager_vacancy, director_vacancy):
+
+        #job_vacancy values added to their self.jab_vacancy
         self.accountant = accountant_vacancy
         self.software_developer = software_developer_vacancy
         self.manager = manager_vacancy
         self.director = director_vacancy
+
+        #create lists for appliers' names
         self.accountant_appliers = []
         self.software_developer_appliers = []
         self.manager_appliers = []
         self.director_appliers = []
 
+    #create add appliers functions
     def add_accountant_appliers(self, name):
         self.accountant_appliers.append(name)
 
@@ -22,6 +27,7 @@ class Vacancy():
         self.director_appliers.append(name)
 
 
+    #how many vacancies left in jobs
 
 
 vacancy = Vacancy(4, 2, 1, 3)

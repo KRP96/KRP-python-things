ac3729e8da24c89ba5368c9042b7ac5389a3bdf7
KRP96 <kavindapeiris1996@gmail.com>
2021-07-09 14:00:58 +0530

    modified variables proccess

--- Job_vacancies.py
+++ Job_vacancies.py
@@ -58,7 +58,7 @@ vacancy = Vacancy(4, 2, 1, 3)
 accountant_appliers = ["Name1", "Name2", "Name3", "Name4","Name5", "Name6"]
 for applier in accountant_appliers:
     if vacancy.add_accountant_appliers(applier):
-        print(f"{applier}, you pass the interview. You won this job.")
+        print(f"{applier}, you pass the interview.")
     else:
         print(f"{applier}, you fail the interview. Good luck for your future.")
 
@@ -66,21 +66,21 @@ for applier in accountant_appliers:
 software_developer_appliers = ["Name7", "Name8", "Name9", "Name10", "Name11"]
 for applier in software_developer_appliers:
     if vacancy.add_software_developer_appliers(applier):
-        print(f"{applier}, you pass the interview. You won this job.")
+        print(f"{applier}, you pass the interview.")
     else:
         print(f"{applier}, you fail the interview. Good luck for your future.")
 
 manager_appliers = ["Name12", "Name13", "Name14", "Name15", "Name16"]
 for applier in manager_appliers:
     if vacancy.add_manager_appliers(applier):
-        print(f"{applier}, you pass the interview. You won this job.")
+        print(f"{applier}, you pass the interview.")
     else:
         print(f"{applier}, you fail the interview. Good luck for your future.")
 
 director_appliers = ["Name17", "Name18", "Name19", "Name20"]   
 for applier in director_appliers:
     if vacancy.add_director_appliers(applier):
-        print(f"{applier}, you pass the interview. You won this job.")
+        print(f"{applier}, you pass the interview.")
     else:
         print(f"{applier}, you fail the interview. Good luck for your future.")
 

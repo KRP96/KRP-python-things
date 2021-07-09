5b704a8a593c936389367637fcf6d71febd0d606
KRP96 <kavindapeiris1996@gmail.com>
2021-07-09 13:42:16 +0530

    create software_developer_appliers variable

--- Job_vacancies.py
+++ Job_vacancies.py
@@ -56,5 +56,5 @@ class Vacancy():
 vacancy = Vacancy(4, 2, 1, 3)
 
 accountant_appliers = ["Name1", "Name2", "Name3", "Name4","Name5", "Name6"]
-
+software_developer_appliers = ["Name7", "Name8", "Name9", "Name10", "Name11"]
         

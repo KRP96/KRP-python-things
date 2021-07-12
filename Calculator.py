89f81708260fda363f35f85128b125b47d38a03d
KRP96 <kavindapeiris1996@gmail.com>
2021-07-12 11:14:48 +0530

    Calculaotr project create

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+from tkinter import*
+import math
+#import parser
+import tkinter.messagebox
+
+root = Tk()
+root.title("Scientific Calculator")
+root.configure(background="powder blue")
+root.resizable(width=False, height=False)
+root.geometry("480x568+0+0")
+
+mainloop()

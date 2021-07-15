8828afe166a339c55be603d4978eae6b09141f98
KRP96 <kavindapeiris1996@gmail.com>
2021-07-15 15:51:14 +0530

    modified

--- Alexa Project/main.py
+++ Alexa Project/main.py
@@ -1,22 +1,61 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')     #voicec change line one
+voices = engine.getProperty('voices')     #voice change line one
 engine.setProperty('voice', voices[1].id)     #voice change line two    '1= female voice / 0= male voice'
-engine.say("Hello, I'm Alexa.")
-engine.say('How can I help you?')
-engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print('listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            print(command)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
-except:
-    pass
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk("Now timw is "+ time)
+
+    elif 'how are you' in command:
+        talk("I'm fine thanks for asking. How are you?")
+
+    elif 'fine too' in command:
+        talk("Ok, dear")
+
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+
+    elif 'who is ' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+
+while True:
+    run_alexa()
